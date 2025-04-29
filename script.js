a1767020108d756245ff7090114cc63e393cfe5f
williamk2s13 <williamsilva.s.21.m.3@gmail.com>
2025-04-29 16:42:10 -0300

    login

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-    
+function Abrirlogin(){
+const abrir = document.querySelector(".open")
+abrir.addEventListener("click",()=>{
+        document.body.insertAdjacentHTML("beforeend",`
+                  <div class="login">
+                       <h1>login</h1>
+            <form class="formulario">
+                <fieldset>
+                    <label></label>
+                    <input type="email" id="email" placeholder="digite seu email">
+                    </fieldset>
+                    <fieldset>
+                        <input type="password" id="senha" placeholder="digite sua senha">
+                </fieldset>
+                
+            </form>
+            <p class="textcadas">caso nao tenha conte <strong>  cadastrase</strong></p>
+            <button class="close">close</button> 
+             </div> 
+
+
+            `)
+            const close = document.querySelector(".close")
+           
+            close.addEventListener("click",()=>{
+               
+                const wrapper = document.querySelector(".login")
+                wrapper.remove()
+            })
+    })
+}
+Abrirlogin()

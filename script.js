1d2bf0744fd8817c33bf16a52260d4c90deb244a
williamk2s13 <williamsilva.s.21.m.3@gmail.com>
2025-04-30 17:21:16 -0300

    login

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function Abrirlogin(){
 const abrir = document.querySelector(".open")
 abrir.addEventListener("click",()=>{
         document.body.insertAdjacentHTML("beforeend",`
+            <div class="fundo">
                   <div class="login">
                        <h1>login</h1>
             <form class="formulario">
@@ -14,10 +15,10 @@ abrir.addEventListener("click",()=>{
                 </fieldset>
                 
             </form>
-            <p class="textcadas">caso nao tenha conte <strong>  cadastrase</strong></p>
+            <p class="textcadas">caso nao tenha conta      <a href="./index2.html">cadastresse</a></p>
             <button class="close">close</button> 
              </div> 
-
+    </div>
 
             `)
             const close = document.querySelector(".close")

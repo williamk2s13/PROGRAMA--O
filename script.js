41bcfc7c1fcc779503c3e3ac6ee64c6936926788
williamk2s13 <williamsilva.s.21.m.3@gmail.com>
2025-05-02 15:34:09 -0300

    login

--- script.js
+++ script.js
@@ -19,15 +19,45 @@ abrir.addEventListener("click",()=>{
             <button class="close">close</button> 
              </div> 
     </div>
-
             `)
             const close = document.querySelector(".close")
            
             close.addEventListener("click",()=>{
                
-                const wrapper = document.querySelector(".login")
+                const wrapper = document.querySelector(".fundo")
                 wrapper.remove()
             })
     })
 }
 Abrirlogin()
+function Abrircadastro(){
+    const abrirc = document.querySelector(".cadastro")
+    abrirc.addEventListener("click",()=>{
+        document.body.insertAdjacentHTML("beforeend",`
+             <div class="fundo">
+        <div class="login">
+             <h1>cadastro</h1>
+  <form class="formulario">
+      <fieldset>
+          <label></label>
+          <input type="email" id="email" placeholder="digite seu email">
+          </fieldset>
+          <fieldset>
+              <input type="password" id="senha" placeholder="digite sua senha">
+      </fieldset>
+ 
+  </form>
+  <p class="textcadas2">caso ja tenha uma conta  faça <a href="./index.html">login</a></p>
+  <button class="close2">close</button> 
+   </div> 
+</div>
+            `
+        )
+        const close2 = document.querySelector(".close2")
+        close2.addEventListener("click",()=>{
+            const wrapper = document.querySelector(".fundo")
+            wrapper.remove()
+        })
+    })
+}
+Abrircadastro()

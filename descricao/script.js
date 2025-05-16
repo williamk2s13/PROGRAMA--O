6d06d7df4bae9e24a3cb720210ce4b83c3b59a17
williamk2s13 <williamsilva.s.21.m.3@gmail.com>
2025-05-16 15:03:11 -0300

    login

--- descricao/script.js
+++ descricao/script.js
@@ -2,6 +2,20 @@ import {cores} from "../pokedex/cores.js"
 async function adcdesc(){
     const pokemon = JSON.parse(localStorage.getItem("pokemon"))
     console.log(pokemon.id,"pokemon")
+    const maxStat = 150;
+const stat = pokemon.stats[0].base_stat;
+const progrecov = `${(stat / maxStat) * 100}%`;
+
+const stata = pokemon.stats[1].base_stat;
+const progrecoa = `${(stata / maxStat) * 100}%`;
+
+   
+const statd = pokemon.stats[2].base_stat;
+const progrecod = `${(statd / maxStat) * 100}%`;
+
+
+const statve = pokemon.stats[3].base_stat;
+const progrecove = `${(statve / maxStat) * 100}%`;
     const desc = document.querySelector(".descricao1")
     desc.insertAdjacentHTML("beforeend",
         `
@@ -14,9 +28,14 @@ async function adcdesc(){
          
         </div>
 
+
+        
         <div class="descricao2">
+        
 <div class="div-p">
+
          <p class="ppoke">
+         
                 <strong><em>Habilidade 1:</em></strong> ${
                   pokemon.abilities[0].ability.name
                 }
@@ -46,14 +65,54 @@ async function adcdesc(){
               pokemon.moves[3] ? pokemon.moves[3].move.name : "Não há"
             }, ${pokemon.moves[4] ? pokemon.moves[4].move.name : "Não há"} 
             </p>
-          <p class="ppoke">
+        
+                         <p class="ppoke">
                         <strong><em>vida:</em></strong> ${
                               pokemon.stats[0].base_stat
                             }
+                            </p>
+
+                            <div class="barra1">
+                                 <div class="progreco"
+                                     style="width:${progrecov}">
+                                </div>
+                            </div>
+
+
+                                    <p class="ppoke">
+                        <strong><em>ataque:</em></strong> ${
+                              pokemon.stats[1].base_stat
+                            }
+                             <div class="barra1">
+                                 <div class="progreco"
+                                     style="width:${progrecoa}">
+                                </div>
+                            </div>
+
+                            </p>
+                                    <p class="ppoke">
+                        <strong><em>defesa:</em></strong> ${
+                              pokemon.stats[2].base_stat
+                            }
+                             <div class="barra1">
+                                 <div class="progreco"
+                                     style="width:${progrecod}">
+                                </div>
+                            </div>
+
+                            </p>
+                                    <p class="ppoke">
+                        <strong><em>velocidade:</em></strong> ${
+                              pokemon.stats[5].base_stat
+                            }
+                             <div class="barra1">
+                                 <div class="progreco"
+                                     style="width:${progrecove}">
+                                </div>
+                            </div>
 
                             </p>
         </div>
-                       
 
         `
     )
